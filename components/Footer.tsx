c86796e5abe121eb43f86c0061b83094a5809972
Ernestoc14 <descreed34@hotmail.com>
2023-07-27 04:14:00 -0500

    Feat Adding the Links Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import { footerLinks } from "@/constants"
 
 const Footer = () => {
     return (
@@ -8,6 +9,35 @@ const Footer = () => {
                 <div className="flex flex-col justify-start items-start gap-6">
                     <Image src='/logo.svg' alt="logo" width={118} height={18} className="object-contain" />
                     <p className="text-base text-gray-700"> 2023 Car Hub Rental. All rights reserved &copy;</p>
+                </div>
+
+                <div className="footer__links">
+                    {footerLinks.map((link) => (
+                        <div className="footer__link" key={link.title}>
+                            <h3 className="font-bold">{link.title}</h3>
+                            {link.links.map((item) => (
+                                <Link 
+                                    className="text-gray-500"
+                                    key={item.title}
+                                    href={item.url}
+                                >
+                                    {item.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="flex items-center justify-between flex-wrap mt-20 border-t border-gray-100 sm:px-16 px-6 py-10">
+                <p>@2023 Car Hub Rental. All Rights Reserved.</p>
+                <div className="footer__copyrights-link">
+                    <Link href="/" className="text-gray-500">
+                        Privacy Policy
+                    </Link>
+                    <Link href="/" className="text-gray-500">
+                        Terms of Use
+                    </Link>
                 </div>
             </div>
         </footer>
