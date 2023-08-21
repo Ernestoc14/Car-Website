f941b80193829a56a326f45b258bb9a53b01a31a
Ernestoc14 <descreed34@hotmail.com>
2023-08-21 12:21:11 -0500

    Feat Adding the Search Model Feature

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,24 +1,56 @@
-"use client"
-import { useState } from 'react'
-import { SearchManufacturer } from './'
+"use client";
+import { useState } from "react";
+import { SearchManufacturer } from "./";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState('')
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
-  const handleSearch = () => {
-    
-  }
+  const handleSearch = () => {};
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__input">
-        <SearchManufacturer 
+      <div className="searchbar__item">
+        <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="model icon"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          placeholder="Model"
+          onChange={(e) => setModel(e.target.value)}
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
