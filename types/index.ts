35ae66a6bf7d6a3031889c4ab281699e0b548842
Ernestoc14 <descreed34@hotmail.com>
2023-08-28 22:14:46 -0500

    Feat ServerSideRendering Searching a Specific car by Model and Make

--- types/index.ts
+++ types/index.ts
@@ -31,10 +31,10 @@ export interface CarProps {
   year: number;
 }
 
-// export interface FilterProps {
-//     manufacturer: string;
-//     year: number;
-//     fuel: string;
-//     limit: number;
-//     model: string;
-// }
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
